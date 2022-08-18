17409072d6b4ff9a581500ce424b81f9dfd333af
tindt2208 <tindt2208@gmail.com>
2022-08-18 14:20:45 +0700

    Update Search bar style and env file

---
+++ src/configs/setting.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable no-undef */
+import devSettings from './dev';
+import stagSettings from './stag';
+import prodSettings from './prod';
+
+let settings = {};
+
+switch (process.env.MODE) {
+  case 'stag':
+    settings = stagSettings;
+    break;
+  case 'prod':
+    settings = prodSettings;
+    break;
+  default:
+    settings = devSettings;
+    break;
+}
+export default settings;
